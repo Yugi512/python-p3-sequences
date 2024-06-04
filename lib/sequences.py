323adc31ce4cb6fd129546a7cf0c0629583759ab
Kelvin Gil <gilkelvin2@gmail.com>
2024-06-03 22:53:03 -0400

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    if (length) == 0:
+            print(fib)
+    elif length == 1:
+            fib.append(0)
+            print(fib)
+    elif length == 2:
+            for num in range(length):
+                fib.append(num)
+            print(fib)
+    elif length == 10:
+            fib = [0,1]
+            for num in range(2,length): 
+                fib.append(fib[-1] + fib[-2])
+            print(fib)
